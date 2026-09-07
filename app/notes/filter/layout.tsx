b1d0ab0f61b62764179b48c627203688f03de1e7
Tetiana <karpenko.tetiana@gmail.com>
2026-09-07 12:02:36 +0200

    updated

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -3,8 +3,8 @@ import css from './LayoutNotes.module.css';
 
 type Props = {
   children: React.ReactNode;
-  sidebar: React.ReactNode;
-  modal: React.ReactNode;
+  sidebar?: React.ReactNode;
+  modal?: React.ReactNode;
 };
 
 const FilterLayout = ({ children, sidebar, modal }: Props) => {
